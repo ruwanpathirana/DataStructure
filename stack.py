93c43bde918db0137ac4dae92f50bd603a0658b4
Ruwan Pathirana <rpathirana836@gmail.com>
2022-12-28 08:33:21 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,30 @@
+#simple stack implementation using python
+stack=[]
+
+#push
+stack.append('p')
+stack.append('q')
+stack.append('r')
+stack.append('s')
+
+print(stack)
+
+#pop
+
+print(stack.pop())
+
+print("Stack after elements are popped:")
+print(stack)
+
+print(stack.pop())
+
+print("Stack after elements are popped:")
+print(stack)
+
+
+stack.pop()
+stack.pop()
+
+print("Stack after elements are popped:")
+print(stack)
+
